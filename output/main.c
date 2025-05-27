07e4ad5919b89aad9ee7b5bd996e3c96d73ef84a
jotta2300 <juniordsc8@gmail.com>
2025-05-27 00:11:47 -0300

    Update main.c

--- output/main.c
+++ output/main.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h> // Para usar srand, rand
-#include <time.h>   // Para usar time para a semente de srand
-#include "Pergunta.h" // Inclui o cabeçalho das funções de pergunta
+#include <stdlib.h>
+#include <time.h>
+#include "Pergunta.h"
 
 int main() {
-    Pergunta *perguntas = NULL; // Ponteiro para o array de perguntas
+    Pergunta *perguntas = NULL;
     int numPerguntas = 0;
 
-    // Inicializa o gerador de numeros aleatorios com base no tempo atual
     srand(time(NULL));
 
-    // Carrega as perguntas do arquivo CSV
+    // Carrega as perguntas do projetobase.csv
     numPerguntas = carregarPerguntasDeCsv("projetobase.csv", &perguntas);
 
     if (numPerguntas > 0) {
         printf("\nIniciando o Show do Milhao!\n");
         printf("Temos %d perguntas carregadas no banco de dados.\n\n", numPerguntas);
 
-        // Realiza o quiz no estilo Show do Milhao
         realizarQuiz(perguntas, numPerguntas);
 
     } else {
-        printf("Nao foi possivel iniciar o quiz. Verifique o arquivo de perguntas e se ha perguntas validas.\n");
+        printf("Nao foi possivel iniciar o quiz. Verifique o arquivo de perguntas.\n");
     }
 
     // Libera a memoria alocada para as perguntas
